a4aefb98130847d3e7c6abd40be528dd5801d179
Randal Kamradt <randykamradt@gmail.com>
2021-08-14 11:50:18 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Event-Driven Vehicle Intake system
+
---
+++ src/main/java/net/kamradtfamily/vehicleevent/AppConfig.java
@@ -0,0 +1,50 @@
+package net.kamradtfamily.vehicleevent;
+
+import org.axonframework.commandhandling.CommandBus;
+import org.axonframework.common.caching.Cache;
+import org.axonframework.common.caching.WeakReferenceCache;
+import org.axonframework.config.EventProcessingConfigurer;
+import org.axonframework.eventhandling.EventBus;
+import org.axonframework.messaging.Message;
+import org.axonframework.messaging.interceptors.LoggingInterceptor;
+import org.axonframework.queryhandling.QueryBus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public LoggingInterceptor<Message<?>> loggingInterceptor() {
+        return new LoggingInterceptor<>();
+    }
+
+    @Autowired
+    public void configureLoggingInterceptorFor(CommandBus commandBus,
+                                               LoggingInterceptor<Message<?>> loggingInterceptor) {
+        commandBus.registerDispatchInterceptor(loggingInterceptor);
+        commandBus.registerHandlerInterceptor(loggingInterceptor);
+    }
+
+    @Autowired
+    public void configureLoggingInterceptorFor(EventBus eventBus, LoggingInterceptor<Message<?>> loggingInterceptor) {
+        eventBus.registerDispatchInterceptor(loggingInterceptor);
+    }
+
+    @Autowired
+    public void configureLoggingInterceptorFor(EventProcessingConfigurer eventProcessingConfigurer,
+                                               LoggingInterceptor<Message<?>> loggingInterceptor) {
+        eventProcessingConfigurer.registerDefaultHandlerInterceptor((config, processorName) -> loggingInterceptor);
+    }
+
+    @Autowired
+    public void configureLoggingInterceptorFor(QueryBus queryBus, LoggingInterceptor<Message<?>> loggingInterceptor) {
+        queryBus.registerDispatchInterceptor(loggingInterceptor);
+        queryBus.registerHandlerInterceptor(loggingInterceptor);
+    }
+
+    @Bean
+    public Cache giftCardCache() {
+        return new WeakReferenceCache();
+    }
+}
